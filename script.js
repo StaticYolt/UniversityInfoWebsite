47d9a332afa2e87e6d6686621cea8ff7d7cdb9a2
StaticYolt <126204653+StaticYolt@users.noreply.github.com>
2025-09-04 16:01:36 -0400

    new update

--- script.js
+++ script.js
@@ -17,19 +17,53 @@ fetch("data.json")
 
 const container = document.getElementById("vertical-marquee");
 const content = document.getElementById("marquee");
+const searchContainer = document.getElementById("search-container");
+const searchBox = document.getElementById("search-box");
 
 content.innerHTML += content.innerHTML;
 let scrollPos = 0;
 let speed = .8;
+let paused = false;
 
 function scrollMarquee() {
-    scrollPos += speed;
-    if (scrollPos >= content.scrollHeight) {
-    console.log(container.style.height)
-    scrollPos = -container.offsetHeight;
+    if(!paused) {
+        scrollPos += speed;
+        if (scrollPos >= content.scrollHeight) {
+            console.log(container.style.height);
+            scrollPos = -container.offsetHeight;
+        }
+        content.style.transform = `translateY(${-scrollPos}px)`;
     }
-    content.style.transform = `translateY(${-scrollPos}px)`;
     requestAnimationFrame(scrollMarquee);
 }
 
 scrollMarquee();
+
+container.addEventListener("click", () => {
+    paused = !paused;
+    if (paused) {
+        // Become scrollable
+        container.style.overflowY = "auto";
+        content.style.transform = ""; // reset transform
+        searchContainer.style.display = "block";
+    } else {
+    // Resume auto-scroll
+        container.scrollTop = 0;
+        container.style.overflow = "hidden";
+        searchContainer.style.display = "none";
+        scrollPos = 0;
+        
+        document.querySelectorAll(".item").forEach(div => {
+            div.style.display = "block";
+        });
+        searchBox.value = "";
+    }
+});
+
+searchBox.addEventListener("input", () => {
+    const query = searchBox.value.toLowerCase();
+    document.querySelectorAll(".item").forEach(div => {
+        const text = div.innerText.toLowerCase();
+        div.style.display = text.includes(query) ? "block" : "none";
+    });
+});
